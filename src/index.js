1ffc10327ab745e0f6756aed7b157675a448fa4d
zaccharie <zaccharie.meddah@gmail.com>
2018-02-13 10:26:27 +0100

    add TalkService

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
+import TalkService from "./common/talk.service.js";
+
 alert('Conférence App démarré');
 
+//Appel findAllSpeakers()
+let talkService = new TalkService();
+
+talkService.findAllSpeakers()
+    .then(
+        speakersFound => {
+            speakersFound.forEach(function(sp){
+                console.log(sp.firstname);
+            })
+        },
+        error => console.log(error)
+    )
